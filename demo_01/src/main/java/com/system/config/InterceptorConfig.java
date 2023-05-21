402b7908875bcc03e24285bdfd3a1f301f8fc613
xu <625969380@qq.com>
2023-05-21 18:50:41 +0800

    修改

--- demo_01/src/main/java/com/system/config/InterceptorConfig.java
+++ demo_01/src/main/java/com/system/config/InterceptorConfig.java
@@ -6,11 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @author xrx
- * @since 2023/4/4
- * description TODO
- */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
